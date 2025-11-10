295a2bbfa90d6f5104fa7979de8e39755450084e
rusu <60366458+rzrusu@users.noreply.github.com>
2025-11-10 14:35:02 +0200

    chore: rename package from com.rzrusu to com.detailo

--- eureka-server/src/main/java/com/detailo/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/detailo/eureka_server/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.rzrusu.eureka_server;
+package com.detailo.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- eureka-server/src/test/java/com/detailo/eureka_server/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/detailo/eureka_server/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.rzrusu.eureka_server;
+package com.detailo.eureka_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
