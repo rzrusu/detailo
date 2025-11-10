ee3fb6d15d09851744c42a1412ddf5c3f4a421df
rusu <60366458+rzrusu@users.noreply.github.com>
2025-11-10 19:11:35 +0200

    refactor: add test property source to application tests

--- identity-service/src/test/java/com/detailo/identity_service/IdentityServiceApplicationTests.java
+++ identity-service/src/test/java/com/detailo/identity_service/IdentityServiceApplicationTests.java
@@ -2,8 +2,13 @@ package com.detailo.identity_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+	"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration",
+	"spring.cloud.config.enabled=false"
+})
 class IdentityServiceApplicationTests {
 
 	@Test
