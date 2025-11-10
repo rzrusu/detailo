d30399442a8bfa653200d126fe38c262ba1fa675
rusu <60366458+rzrusu@users.noreply.github.com>
2025-11-10 19:11:23 +0200

    cleanup: remove unused SecurityFilterChain import

--- identity-service/src/main/java/com/detailo/identity_service/config/SecurityConfig.java
+++ identity-service/src/main/java/com/detailo/identity_service/config/SecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
